234a1376f4da1508b32097e497eec047126301bb
George Jieh <george.jieh@gmail.com>
2024-12-24 02:52:51 -0500

    Add comments.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+// Importing React and ReactDOM to render the application
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Importing the main App component and global styles
+import App from './App.jsx';
+import './index.css';
+
+// Importing the Provider component from react-redux to connect Redux with React
+import { Provider } from 'react-redux';
+
+// Importing the Redux store created in store.js
+import store from './store.js';
+
+// Rendering the React application and wrapping it with the Provider component
+// The Provider component enables the Redux store to be accessible throughout the app
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
