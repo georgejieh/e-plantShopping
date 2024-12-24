e0bf49bd63e2f8ea0119ec994fba557486334989
George Jieh <george.jieh@gmail.com>
2024-12-24 02:51:26 -0500

    Add comments.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,16 @@
+// Importing the configureStore function from Redux Toolkit to create the Redux store
 import { configureStore } from '@reduxjs/toolkit';
+
+// Importing the cartReducer from the CartSlice.js file to manage the "cart" slice of the state
 import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+
+// Configuring the Redux store
+// The reducer object specifies the state slices and their corresponding reducers
+const store = configureStore({
+  reducer: {
+    cart: cartReducer, // The "cart" slice of state is managed by cartReducer
+  },
 });
-export default store
+
+// Exporting the configured store to be used throughout the application
+export default store;
